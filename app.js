173d2bdbacf4580f066e37828563cbb60ef02c35
lucascmullin <lucascmullin@gmail.com>
2024-07-21 12:56:27 +0000

    Script to create a set of domains from a base name.

---
+++ app.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const blockListBaseFolder = `./base/`;
+
+fs.readdirSync(blockListBaseFolder).forEach(file => {
+    const filePathInput = `${blockListBaseFolder}${file}`;
+    const filePathOutput = `./${file}`;
+    generateBlockList(filePathInput, filePathOutput);
+});
+
+function generateBlockList(filePathInput, filePathOutput) {
+    const initialUrls = ['www', 'pt'];
+    const finalUrls = ['com', 'com.br'];
+
+    var lineReader = require('readline').createInterface({
+        input: fs.createReadStream(filePathInput)
+    });
+
+    var blockList = [];
+    lineReader.on('line', function (line) {
+        finalUrls.forEach(finalUrl => {
+            blockList.push([line, finalUrl].join('.'));
+        });
+        initialUrls.forEach(initialUrl => {
+            finalUrls.forEach(finalUrl => {
+                blockList.push([initialUrl, line, finalUrl].join('.'));
+            });
+        });
+    });
+
+    lineReader.on('close', function () {
+        fs.writeFileSync(filePathOutput, blockList.join('\n'));
+    });
+}
